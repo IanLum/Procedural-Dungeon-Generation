8133a1db84b96d8f718b22c8508c17a03f30deed
Ian Lum <ianlum314@gmail.com>
2024-04-18 10:37:31 -0400

    add return type

--- main.gd
+++ main.gd
@@ -29,20 +29,40 @@ func _draw():
 
 func generate_rooms():
 	root.split(splits_selector.value)
+	tile_map.clear()
 	place_room_tiles()
+	place_hallway_tiles()
 	queue_redraw()
 
 func place_room_tiles():
-	tile_map.clear()
 	for leaf in root.get_leaves():
 		for x in range(1, leaf.size.x - 1):
 			for y in range(1, leaf.size.y - 1):
-				tile_map.set_cell(
-					0,
-					Vector2i(x + leaf.position.x, y + leaf.position.y),
-					1,
-					Vector2i(0, 0)
-				)
+				place_tile(x + leaf.position.x, y + leaf.position.y)
+
+func place_hallway_tiles():
+	var queue: Array[Branch] = [root]
+	while not queue.is_empty():
+		var curr: Branch = queue.pop_front()
+		var left := curr.left
+		var right := curr.right
+		if not (left and right):
+			continue
+		
+		queue.append_array([left, right])
+		var left_center := left.center()
+		var right_center := right.center()
+		if left_center.x == right_center.x:
+			# vertical hallway
+			for y in range(right_center.y - left_center.y):
+				place_tile(left_center.x, left_center.y + y)
+		else:
+			# horizontal hallway
+			for x in range(right_center.x - left_center.x):
+				place_tile(left_center.x + x, left_center.y)
+
+func place_tile(x, y):
+	tile_map.set_cell(0, Vector2i(x, y), 1, Vector2i(0, 0))
 
 func _on_reroll_pressed():
 	generate_rooms()
