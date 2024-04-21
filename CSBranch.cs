c20005da1b89b974878b186db40cedffb7d5bf0b
Ian Lum <ianlum314@gmail.com>
2024-04-21 18:36:31 -0400

    impl split

--- CSBranch.cs
+++ CSBranch.cs
@@ -19,13 +19,49 @@ public partial class CSBranch : Node
 	{
 		if ((left is null) && (right is null))
 		{
-			var output = new Array<CSBranch>();
-			output.Add(this);
+			var output = new Array<CSBranch>() { this };
 			return output;
 		}
 		else
 		{
 			return left.GetLeaves() + right.GetLeaves();
 		}
+	}
+
+	public void Split(int splits)
+	{
+		if (splits == 0) return;
+		Random rng = new Random();
+		double split_percent = rng.Next(30, 70) / 100.0;
+
+		if (size.X > size.Y)
+		{
+			// Vertical line split
+			int left_width = (int)(size.X * split_percent);
+			left = new CSBranch(
+				position,
+				new Vector2I(left_width, size.Y)
+			);
+			right = new CSBranch(
+				new Vector2I(position.X + left_width, position.Y),
+				new Vector2I(size.X - left_width, size.Y)
+			);
+		}
+		else
+		{
+			// Horizontal line split
+			int left_height = (int)(size.Y * split_percent);
+			left = new CSBranch(
+				position,
+				new Vector2I(size.X, left_height)
+			);
+			right = new CSBranch(
+				new Vector2I(position.X, position.Y + left_height),
+				new Vector2I(size.X, size.Y - left_height)
+			);
+		}
+
+		left.Split(splits - 1);
+		right.Split(splits - 1);
 	}
 }
