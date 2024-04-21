a52fa41bb457baaab4d1d35106778233fe51e0c7
Ian Lum <ianlum314@gmail.com>
2024-04-21 19:52:04 -0400

    config main to work with gd or cs branch

--- main.gd
+++ main.gd
@@ -1,24 +1,26 @@
 extends Node2D
 
 const TILE_SIZE := 16
-const CSBranch: CSharpScript = preload("res://CSBranch.cs")
+const CSBranch: CSharpScript = preload ("res://CSBranch.cs")
 
 @onready var tile_map = $TileMap
 @onready var splits_selector = $CanvasLayer/NumSplits
 
+var use_csharp = true
 var root: Branch
+var cs_root
 
 func _ready():
-	var node = CSBranch.new(Vector2i(0,0), Vector2i(2,2))
-	print(node)
-	root = Branch.new(
-		Vector2i(0, 0),
-		Vector2i(60, 30)
-	)
+	root = Branch.new(Vector2i(0, 0), Vector2i(60, 30))
+	cs_root = CSBranch.new(Vector2i(0, 0), Vector2i(60, 30))
 	full_generate()
 
 func _draw():
-	for leaf in root.get_leaves():
+	draw_regions()
+
+func draw_regions():
+	var leaves = cs_root.GetLeaves() if use_csharp else root.get_leaves()
+	for leaf in leaves:
 		draw_rect(
 			Rect2(
 				leaf.position.x * TILE_SIZE, # x
@@ -31,30 +33,37 @@ func _draw():
 		)
 
 func full_generate():
-	root.split(splits_selector.value)
+	partition()
 	tile_map.clear()
 	place_room_tiles()
 	place_hallway_tiles()
 	queue_redraw()
 
+func partition():
+	if use_csharp:
+		cs_root.Split(splits_selector.value)
+	else:
+		root.split(splits_selector.value)
+
 func place_room_tiles():
-	for leaf in root.get_leaves():
+	var leaves = cs_root.GetLeaves() if use_csharp else root.get_leaves()
+	for leaf in leaves:
 		for x in range(1, leaf.size.x - 1):
 			for y in range(1, leaf.size.y - 1):
 				place_tile(x + leaf.position.x, y + leaf.position.y)
 
 func place_hallway_tiles():
-	var queue: Array[Branch] = [root]
+	var queue = [cs_root] if use_csharp else [root]
 	while not queue.is_empty():
-		var curr: Branch = queue.pop_front()
-		var left := curr.left
-		var right := curr.right
+		var curr = queue.pop_front()
+		var left = curr.left
+		var right = curr.right
 		if not (left and right):
 			continue
 		
 		queue.append_array([left, right])
-		var left_center := left.center()
-		var right_center := right.center()
+		var left_center: Vector2i = left.Center() if use_csharp else left.center()
+		var right_center: Vector2i = right.Center() if use_csharp else right.center()
 		if left_center.x == right_center.x:
 			# vertical hallway
 			for y in range(right_center.y - left_center.y):
@@ -70,16 +79,17 @@ func place_tile(x, y):
 func _on_reroll_pressed():
 	full_generate()
 
-
 func _on_partition_pressed():
-	root.split(splits_selector.value)
+	partition()
 	tile_map.clear()
 	queue_redraw()
-
 
 func _on_place_rooms_pressed():
 	place_room_tiles()
 
-
 func _on_place_hallways_pressed():
 	place_hallway_tiles()
+
+
+func _on_use_c_sharp_toggled(toggled_on):
+	use_csharp = toggled_on
