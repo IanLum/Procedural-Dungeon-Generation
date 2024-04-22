1053f6b5d18273f3683c1480327bcb3c35da40d8
Ian Lum <ianlum314@gmail.com>
2024-04-22 11:30:34 -0400

    place hallway tiles

--- terrain.gd
+++ terrain.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 const TILE_SIZE := 16
+const PADDING := 1
 
 @onready var tile_map = $TileMap
 
@@ -12,25 +13,6 @@ func _ready():
 		Vector2i(60, 30)
 	)
 	full_generate()
-#	tile_map.set_cell(0, Vector2i(0, 0), 0, Vector2i(0, 0))
-#	tile_map.set_cells_terrain_connect(
-#		0,
-#		[Vector2i(0, 0)],
-#		0,
-#		0
-#	)
-#	tile_map.set_cells_terrain_path(
-#		0,
-#		[Vector2i(0, 2), Vector2i(1, 2), Vector2i(0, 3), Vector2i(1, 3)],
-#		0,
-#		0
-#	)
-#	tile_map.set_cells_terrain_connect(
-#		0,
-#		[Vector2i(0, -2), Vector2i(1, -2), Vector2i(0, -3), Vector2i(1, -3)],
-#		0,
-#		0
-#	)
 
 func _draw():
 	for leaf in root.get_leaves():
@@ -46,9 +28,10 @@ func _draw():
 		)
 
 func full_generate():
-	root.split(5)
+	root.split(4)
 	tile_map.clear()
 	place_room_tiles()
+	place_hallway_tiles()
 	queue_redraw()
 
 func place_terrain(tiles: Array[Vector2i]):
@@ -60,7 +43,32 @@ func place_room_tiles():
 	for leaf in root.get_leaves():
 		print(leaf.position, leaf.size)
 		var tiles: Array[Vector2i] = []
-		for x in range(1, leaf.size.x - 1):
-			for y in range(1, leaf.size.y - 1):
+		for x in range(PADDING, leaf.size.x - PADDING):
+			for y in range(PADDING, leaf.size.y - PADDING):
 				tiles.append(Vector2i(x + leaf.position.x, y + leaf.position.y))
+		place_terrain(tiles)
+
+func place_hallway_tiles():
+	var queue: Array[Branch] = [root]
+	while not queue.is_empty():
+		var curr: Branch = queue.pop_front()
+		var left := curr.left
+		var right := curr.right
+		if not (left and right):
+			continue
+		
+		queue.append_array([left, right])
+		var left_center := left.center()
+		var right_center := right.center()
+		var tiles: Array[Vector2i] = []
+		if left_center.x == right_center.x:
+			# vertical hallway
+			for y in range(right_center.y - left_center.y):
+				for x in range(left_center.x - 1, left_center.x + 2):
+					tiles.append(Vector2i(x, left_center.y + y))
+		else:
+			# horizontal hallway
+			for x in range(right_center.x - left_center.x):
+				for y in range(left_center.y - 1, left_center.y + 2):
+					tiles.append(Vector2i(left_center.x + x, y))
 		place_terrain(tiles)
